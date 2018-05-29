253a5ae84e824e837ad2c03410157cf884142881
Youngjoon Lee <yj.lee@yonsei.ac.kr>
2018-05-29 13:41:18 +0900

    bug fix

--- ekonlpy/data/tagset.py
+++ ekonlpy/data/tagset.py
@@ -239,6 +239,7 @@ chk_tags = {
     ('NNG', 'NNG', 'VA', 'IC'): 'NNG',
     ('NNG', 'NNG', 'VA', 'NNG'): 'NNG',
     ('NNG', 'NNG', 'VCP', 'VA', 'NNG'): 'NNG',
+    ('NNG', 'NNG', 'VV'): 'NNG',
     ('NNG', 'NNG', 'VV', 'NNG'): 'NNG',
     ('NNG', 'NNG', 'XSN'): 'NNG',
     ('NNG', 'NNG', 'XSN', 'NNG'): 'NNG',
