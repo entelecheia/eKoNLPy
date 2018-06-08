b36413bf36281f84a751f42b6917bfba206d59c9
Youngjoon Lee <yj.lee@yonsei.ac.kr>
2018-06-08 22:42:08 +0900

    bug fix

--- ekonlpy/tag/_mecab.py
+++ ekonlpy/tag/_mecab.py
@@ -59,7 +59,7 @@ class Mecab:
         self._dictionary.add_dictionary(load_dictionary('%s/ADVERBES.txt' % directory), 'MAG')
         self._dictionary.add_dictionary(load_dictionary('%s/UNIT.txt' % directory), 'NNBC')
         # self._dictionary.add_dictionary(load_dictionary('%s/FOREIGN_TERMS.txt' % directory), 'SL')
-        self._dictionary.add_dictionary(load_dictionary('%s/ECON_PHRASES.txt' % directory), 'NNG')
+        # self._dictionary.add_dictionary(load_dictionary('%s/ECON_PHRASES.txt' % directory), 'NNG')
         self._dictionary.add_dictionary(load_dictionary('%s/SECTOR.txt' % directory), 'NNG')
         if use_polarity_phrases:
             self._dictionary.add_dictionary(load_dictionary('%s/POLARITY_PHRASES.txt' % directory), 'NNG')
