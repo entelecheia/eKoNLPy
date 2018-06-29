7c22d75261b65af565259d62e6ad46eef3e17250
Youngjoon Lee <yj.lee@yonsei.ac.kr>
2018-06-29 16:30:12 +0900

    review terms and synonyms change custum pos routine completely

--- ekonlpy/etag/_template.py
+++ ekonlpy/etag/_template.py
@@ -2,7 +2,7 @@ from ekonlpy.data.tagset import skip_chk_tags, skip_tags, nouns_tags, pass_tags
 
 
 class ETagger:
-    def __init__(self, dictionary, max_ngram=6):
+    def __init__(self, dictionary, max_ngram=10):
         self.dictionary = dictionary
         self.max_ngram = max_ngram
         self.skip_chk_tags = skip_chk_tags
@@ -19,85 +19,70 @@ class ETagger:
             self.skip_tags.update(tags)
 
     def pos(self, tokens):
-        def ctagger(ctokens, ngram, cnouns_tags, cpass_tags, cskip_chk_tags, cskip_tags, cdictionary):
+        def ctagger(ctokens, max_ngram, cnouns_tags, cpass_tags, cskip_chk_tags, cskip_tags, cdictionary):
 
             tokens_org = ctokens
             num_tokens = len(ctokens)
             tokens_new = []
-            i = ngram - 1
-            # if length of tokens is less than n, bypass and return original tokens
-            if num_tokens < ngram:
-                return ctokens
+            ipos = 0
 
-            while i < num_tokens:
-                tmp_tags = []
-                for j in range(ngram):
-                    tmp_tags.append('NNG'
-                                    if tokens_org[i - ngram + j + 1][1] in cnouns_tags
-                                    else tokens_org[i - ngram + j + 1][1])
-                tmp_tags = tuple(tmp_tags)
-
-                if tmp_tags not in cpass_tags:
-                    new_word = ''
-                    for j in range(ngram):
-                        new_word += tokens_org[i - ngram + j + 1][0]
-                    dict_tag = cdictionary.get_tags(new_word.lower())
-                    if dict_tag:
-                        tokens_new.append((new_word, dict_tag))
-                        i += ngram
-                        # if position of token reachs to the end, append remaining tokens
-                        if i >= num_tokens:
-                            for j in range(ngram - 1):
-                                pos = i - ngram + j + 1
-                                if pos < num_tokens:
-                                    tokens_new.append(tokens_org[pos])
+            while ipos < num_tokens:
+                word_found = False
+                for ngram in range(max_ngram, 1, -1):
+                    # if found a word from the dictionary, skip for loop
+                    if word_found or ipos + ngram > num_tokens - 1:
                         continue
 
-                if tmp_tags in cskip_chk_tags.keys():
-                    new_word = ''
-                    num_word = ''
+                    tmp_tags = []
                     for j in range(ngram):
-                        if tmp_tags[j] not in cskip_tags:
-                            new_word += tokens_org[i - ngram + j + 1][0]
-                        if tmp_tags[j] == 'SN':
-                            num_word += 'n'
-                        else:
-                            num_word += tokens_org[i - ngram + j + 1][0]
-                    dict_tag = cdictionary.get_tags(num_word.lower())
-                    if dict_tag:
-                        new_word = num_word
-                    else:
+                        tmp_tags.append('NNG'
+                                        if tokens_org[ipos + j][1] in cnouns_tags
+                                        else tokens_org[ipos + j][1])
+                    tmp_tags = tuple(tmp_tags)
+
+                    if tmp_tags not in cpass_tags:
+                        new_word = ''
+                        for j in range(ngram):
+                            new_word += tokens_org[ipos + j][0]
                         dict_tag = cdictionary.get_tags(new_word.lower())
-                    if dict_tag:
-                        new_tag = dict_tag if cskip_chk_tags[tmp_tags] == 'NNG' else cskip_chk_tags[tmp_tags]
-                        tokens_new.append((new_word, new_tag))
-                        i += ngram
-                        # if position of token reachs to the end, append remaining tokens
-                        if i >= num_tokens:
-                            for j in range(ngram - 1):
-                                pos = i - ngram + j + 1
-                                if pos < num_tokens:
-                                    tokens_new.append(tokens_org[pos])
-                        continue
+                        if dict_tag:
+                            tokens_new.append((new_word, dict_tag))
+                            ipos += ngram
+                            word_found = True
 
-                tokens_new.append(tokens_org[i - ngram + 1])
-                # if position of token reachs to the end, append remaining tokens
-                if i >= num_tokens - 1:
-                    for j in range(ngram - 1):
-                        pos = i - ngram + j + 2
-                        if pos < num_tokens:
-                            tokens_new.append(tokens_org[pos])
-                i += 1
+                    if not word_found and tmp_tags in cskip_chk_tags.keys():
+                        new_word = ''
+                        num_word = ''
+                        for j in range(ngram):
+                            if tmp_tags[j] not in cskip_tags:
+                                new_word += tokens_org[ipos + j][0]
+                            if tmp_tags[j] == 'SN':
+                                num_word += 'n'
+                            else:
+                                num_word += tokens_org[ipos + j][0]
+                        dict_tag = cdictionary.get_tags(num_word.lower())
+                        if dict_tag:
+                            new_word = num_word
+                        else:
+                            dict_tag = cdictionary.get_tags(new_word.lower())
+                        if dict_tag:
+                            new_tag = dict_tag if cskip_chk_tags[tmp_tags] == 'NNG' else cskip_chk_tags[tmp_tags]
+                            tokens_new.append((new_word, new_tag))
+                            ipos += ngram
+                            word_found = True
+
+                # if not found a word from the dictionary, add current token
+                if not word_found:
+                    tokens_new.append(tokens_org[ipos])
+                    ipos += 1
 
             return tokens_new
 
         tokens = [(w.strip(), self.dictionary.check_tag(w.strip(), t))
                   for w, t in tokens]
 
-        for x in range(3):
-            for t in range(2, self.max_ngram - x):
-                tokens = ctagger(tokens, t, self.nouns_tags, self.pass_tags,
-                                 self.skip_chk_tags, self.skip_tags, self.dictionary)
+        tokens = ctagger(tokens, self.max_ngram, self.nouns_tags, self.pass_tags,
+                         self.skip_chk_tags, self.skip_tags, self.dictionary)
 
         tokens = [(w, self.dictionary.check_tag(w, t))
                   for w, t in tokens]
