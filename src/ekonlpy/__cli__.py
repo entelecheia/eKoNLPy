6ae0a8189f977339cbb2e6fb00cf61abb7e3638a
Young Joon Lee <entelecheia@hotmail.com>
2023-07-24 12:40:16 +0900

    feat(ekonlpy): add command line interface

---
+++ src/ekonlpy/__cli__.py
@@ -0,0 +1,38 @@
+"""Command line interface for eKoNLPy"""
+
+# Importing the libraries
+
+import click
+
+from ._version import __version__
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.version_option(__version__)
+@click.option(
+    "--tagger",
+    "-t",
+    show_default=True,
+    default="ekonlpy",
+    help="The tagger to use. [ekonlpy|mecab]]",
+)
+@click.option("--input", "-i", help="The input text to tag.")
+@click.pass_context
+def main(ctk, tagger, input):
+    """This is the command line interface for eKoNLPy.
+
+    It is used to tag Korean text with a Korean morphological analyzer.
+    """
+    # Print a message to the user.
+    if input:
+        click.echo(f"Tagging {input} with {tagger}...")
+    else:
+        # Print usage message to the user.
+        click.echo(ctk.get_help())
+
+
+# main function for the main module
+if __name__ == "__main__":
+    main()
