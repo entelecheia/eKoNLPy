c4a2ce851746fe62b11bb8b2e70a76922e22d4c2
Young Joon Lee <entelecheia@hotmail.com>
2023-08-26 06:28:40 +0900

    chore(test_sentiment): add print statements for token debugging

--- tests/ekonlpy/test_sentiment.py
+++ tests/ekonlpy/test_sentiment.py
@@ -5,6 +5,7 @@ def test_sentiment():
     text = "금통위는 따라서 물가안정과 병행, 경기상황에 유의하는 금리정책을 펼쳐나가기로 했다고 밝혔다."
     tokens = mpko.tokenize(text)
     score = mpko.get_score(tokens)
+    print(tokens)
     print(score)
     # {'Positive': 1, 'Negative': 0, 'Polarity': 0.9999990000010001, 'Subjectivity': 0.9999990000010001}
     assert score["Positive"] == 1 and score["Negative"] == 0
@@ -15,6 +16,7 @@ def test_sentiment():
     tokens = mpck.tokenize(text)
     ngrams = mpck.ngramize(tokens)
     score = mpck.classify(tokens + ngrams, intensity_cutoff=1.5)
+    print(tokens)
     print(score)
     # {'Polarity': -0.972986080355786, 'Intensity': 73.03049854822524, 'Pos score': 0.013506959822107319, 'Neg score': 0.9864930401778934}
     assert score["Polarity"] < 0
